fff2b8dd31571e66a7f9b0168b15dcf609a4e2af
elimossinary <me@frankmitchell.org>
2012-09-06 07:15:42 -0400

    Adds simple drag and drop support to the player's pieces.
    There's no target hilights, no snap back to the original position, no IE
    support, or any other shiny bits. But you can drag and drop stuff.

--- js/mentat.js
+++ js/mentat.js
@@ -11,6 +11,55 @@ function html(id, text) {
   $(id).innerHTML = text;
 }
 
+var dragDrop = {
+  initialMouseX: null,
+  initialMouseY: null,
+  startX: null,
+  startY: null,
+  draggedObject: null,
+
+  bind: function (element) {
+    if (typeof element === "string") {
+      element = $(element);
+    }
+    element.onmousedown = dragDrop.startDragMouse;
+  },
+
+  startDragMouse: function (e) {
+    dragDrop.startDrag(this);
+    dragDrop.initialMouseX = e.clientX;
+    dragDrop.initialMouseY = e.clientY;
+    document.addEventListener("mousemove", dragDrop.dragMouse, false);
+    document.addEventListener("mouseup", dragDrop.releaseElement, false);
+    return false;
+  },
+
+  startDrag: function (object) {
+    if (dragDrop.draggedObjct) {
+      dragDrop.releaseElement();
+    }
+    object.style.left = "0";
+    object.style.top = "0";
+    dragDrop.startX = parseInt(object.style.left);
+    dragDrop.startY = parseInt(object.style.top);
+    dragDrop.draggedObject = object;
+  },
+
+  dragMouse: function (e) {
+    var dx = e.clientX - dragDrop.initialMouseX;
+    var dy = e.clientY - dragDrop.initialMouseY;
+    dragDrop.draggedObject.style.left = dragDrop.startX + dx + "px";
+    dragDrop.draggedObject.style.top = dragDrop.startY + dy + "px";
+    return false;
+  },
+
+  releaseElement: function () {
+    document.removeEventListener("mousemove", dragDrop.dragMouse, false);
+    document.removeEventListener("mouseup", dragDrop.releaseElement, false);
+    dragDrop.draggedObject = null;
+  }
+}
+
 function shuffle(array) {
   var i, j, temp;
   for (i = array.length - 1; i > 0; i -= 1) {
@@ -30,6 +79,7 @@ shuffle(pieces);
 
 for (i = 0; i < 8; i += 1) {
   html("piece" + i, pieces[i]);
+  dragDrop.bind("piece" + i);
 }
 
 }());
