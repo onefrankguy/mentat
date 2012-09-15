d52e5aa309cbed6313bccc2ee51a963655afd440
elimossinary <me@frankmitchell.org>
2012-09-15 10:59:20 -0400

    Makes JSLint run as part of the build.

--- rakefile.rb
+++ rakefile.rb
@@ -3,13 +3,26 @@ require 'rake'
 task :default => :test
 
 desc "Makes sure the code isn't to big?"
-task :test do
+task :test => :jslint do
   old_size = File.exists?('mentat.zip') ? File.size('mentat.zip') : 0
   sh 'zip -r mentat . -i@manifest.txt'
   new_size = File.size('mentat.zip')
   puts "Old size: #{old_size} bytes (#{percent(old_size)}%)"
   puts "New size: #{new_size} bytes (#{percent(new_size)}%)"
   fail 'Zip file too big!' if new_size > 10 * 1024
+end
+
+desc 'Runs JSLint on the code'
+task :jslint do
+  lint ['document'], 'js/jquery.js'
+  lint ['document', 'jQuery'], 'js/dnd.js'
+  lint ['document', 'jQuery', 'DragDrop'], 'js/mentat.js'
+end
+
+def lint predefs, file
+  predefs.map! { |var| "--predef #{var}" }
+  predefs = predefs.join(' ')
+  sh "jslint --white #{predefs} #{file}"
 end
 
 def percent size
